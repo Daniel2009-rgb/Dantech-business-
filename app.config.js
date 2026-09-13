bab90d3f3949e4f8541286d39ec16d28e0ce7718
Daniel2009-rgb <danieldede856@yahoo.com>
2026-09-13 03:24:19 +0300

    Update app.config.js for deep links and comments
    Added scheme for deep links and cleaned up comments.

--- app.config.js
+++ app.config.js
@@ -1,18 +1,24 @@
-
 export default {
   name: "Dantech Daraja",
   slug: "dantech business",
   version: "1.0.0",
+  scheme: "dantech", // ya kusaidia deeplinks
+  
   android: {
-    permissions: ["RECORD_AUDIO"] // HII NI MUHIMU KWA VOICE
+    package: "com.dantech.daraja", // HII NDIO INAYOKOSEA
+    versionCode: 1,
+    permissions: ["RECORD_AUDIO"] // Kwa Voice
   },
+
   ios: {
+    bundleIdentifier: "com.dantech.daraja", // Kwa iPhone
     infoPlist: {
       NSSpeechRecognitionUsageDescription: "App inahitaji kurekodi sauti yako",
       NSMicrophoneUsageDescription: "App inahitaji kutumia microphone"
     }
   },
+
   plugins: [
-    "@react-native-voice/voice" // Acha hivi tu, bila vitu ndani
+    "@react-native-voice/voice"
   ]
 };
