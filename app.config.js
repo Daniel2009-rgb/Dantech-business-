3d482a92f7b3975dbe7ca1e1aee26694deaba650
Daniel2009-rgb <danieldede856@yahoo.com>
2026-09-13 03:15:46 +0300

    Update app.config.js for audio permissions
    Added permissions for audio recording on Android and updated iOS infoPlist for microphone usage.

--- app.config.js
+++ app.config.js
@@ -1,13 +1,18 @@
+
 export default {
   name: "Dantech Daraja",
   slug: "dantech business",
   version: "1.0.0",
+  android: {
+    permissions: ["RECORD_AUDIO"] // HII NI MUHIMU KWA VOICE
+  },
+  ios: {
+    infoPlist: {
+      NSSpeechRecognitionUsageDescription: "App inahitaji kurekodi sauti yako",
+      NSMicrophoneUsageDescription: "App inahitaji kutumia microphone"
+    }
+  },
   plugins: [
-    [
-      "@react-native-voice/voice",
-      {
-        "microphonePermission": "Tafadhali ruhusu mic ili tuzungumze na app"
-      }
-    ]
+    "@react-native-voice/voice" // Acha hivi tu, bila vitu ndani
   ]
 };
