a4d29387d6629e3743fa783a8ae31fbe16c7740f
Daniel2009-rgb <danieldede856@yahoo.com>
2026-09-13 03:06:59 +0300

    Add app configuration for Dantech Daraja

---
+++ app.config.js
@@ -0,0 +1,13 @@
+export default {
+  name: "Dantech Daraja",
+  slug: "dantech business",
+  version: "1.0.0",
+  plugins: [
+    [
+      "@react-native-voice/voice",
+      {
+        "microphonePermission": "Tafadhali ruhusu mic ili tuzungumze na app"
+      }
+    ]
+  ]
+};
